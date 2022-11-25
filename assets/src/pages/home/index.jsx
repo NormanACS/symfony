7a8935214d0bb4801ddf80693277bcfc816863ae
NormanACS <normanlisozzi@gmail.com>
2022-11-25 11:39:37 +0100

    Update API

--- assets/src/pages/home/index.jsx
+++ assets/src/pages/home/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "axios";
+import { checkPropTypes } from "prop-types";
 
 const Home = () => {
+
+    const onSubmit = () => {
+        axios.get("http://localhost:8000/api/users")
+            .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+        console.log("clicked");
+    };
+
     return (
         <div>
             <h1>Hello HomePage !</h1>
+            <button type="button" onClick={() => onSubmit()}>Envoyer</button>
         </div>
     )
 }
