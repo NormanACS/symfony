f0e85e288f24bd5288420ef6cbe9ebe68d66f8ef
NormanACS <normanlisozzi@gmail.com>
2022-12-19 09:53:20 +0100

    Update entity

--- backend/src/Entity/Client.php
+++ backend/src/Entity/Client.php
@@ -4,8 +4,8 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ClientRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
 #[ApiResource]
@@ -16,11 +16,8 @@ class Client
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $full_name = null;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $email = null;
 
     public function getId(): ?int
     {
@@ -35,18 +32,6 @@ class Client
     public function setFullName(?string $full_name): self
     {
         $this->full_name = $full_name;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(?string $email): self
-    {
-        $this->email = $email;
 
         return $this;
     }
